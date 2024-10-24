2082d1c835a430f04acf7dc1ab6c41f548879bb1
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-10-24 14:07:13 +0200

    Fixed Typescript rules

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: false,
+    tsconfigPath: "./tsconfig.json",
+  },
 };
 
 module.exports = nextConfig;
