bb4f2e7b1cbb6e56464fc6a8ee81d335a5772570
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-06-02 20:34:09 +0200

    Sidebar cleanup

--- src/components/sidebar/nav-user.tsx
+++ src/components/sidebar/nav-user.tsx
@@ -96,21 +96,6 @@ export function NavUser({
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <Sparkles />
-                Upgrade to Pro
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <BadgeCheck />
-                Account
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <CreditCard />
-                Billing
-              </DropdownMenuItem>
               <DropdownMenuItem
                 onClick={() => router.push("/account/settings")}
               >
