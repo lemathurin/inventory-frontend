0982a986880d9dcea43418add40054e912542fb0
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-06-02 22:56:02 +0200

    useUpdateHome hook

---
+++ src/domains/home/hooks/useUpdateHome.ts
@@ -0,0 +1,17 @@
+import axios from "@/lib/axios";
+import { HOME_ENDPOINTS } from "../endpoints";
+
+export function useUpdateHome() {
+  return async (
+    homeId: string,
+    name?: string,
+    address?: string,
+  ): Promise<void> => {
+    try {
+      await axios.patch(`${HOME_ENDPOINTS.home}/${homeId}`, { name, address });
+    } catch (error) {
+      console.error("Could not fetch home", error);
+      throw error;
+    }
+  };
+}
