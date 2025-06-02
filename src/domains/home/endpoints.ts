c18e40aa40033537b9695f6efd17c327e6d3ee46
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-06-02 20:48:55 +0200

    Home endpoints

---
+++ src/domains/home/endpoints.ts
@@ -0,0 +1,8 @@
+export const HOME_ENDPOINTS = {
+  home: "/homes/:homeId",
+  createHome: "/homes/",
+  getRoomsByHomeId: "/homes/:homeId/rooms",
+  invite: "/homes/:homeId/invites",
+  deleteInvite: "/homes/:homeId/invites/:inviteId",
+  acceptInvite: "/homes/invites/accept",
+} as const;
