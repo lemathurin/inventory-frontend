d17b73301837fe02bc943a7c8c9bb2a375ff5f1d
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-06-10 20:49:24 +0200

    Added AppHeader to CreateItemPage

--- src/app/(app)/home/[homeId]/create-item/page.tsx
+++ src/app/(app)/home/[homeId]/create-item/page.tsx
@@ -1,9 +1,13 @@
 import CreateItemForm from "@/components/CreateItemForm";
+import { AppHeader } from "@/components/AppHeader";
 
 export default function CreateItemPage() {
   return (
-    <div className="m-4">
-      <CreateItemForm />
-    </div>
+    <>
+      <AppHeader breadcrumbs={[{ label: "Create an item", isCurrent: true }]} />
+      <div className="m-4">
+        <CreateItemForm />
+      </div>
+    </>
   );
 }
