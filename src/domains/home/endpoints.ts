90ba038037ee45fc0c46c2de7bd46c6b886c06f3
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2025-06-11 22:02:14 +0200

    useGetHomeUsers.ts hook

--- src/domains/home/endpoints.ts
+++ src/domains/home/endpoints.ts
@@ -5,4 +5,6 @@ export const HOME_ENDPOINTS = {
   invite: "/homes/:homeId/invites",
   deleteInvite: "/homes/:homeId/invites/:inviteId",
   acceptInvite: "/homes/invites/accept",
+  getHomeUsers: "/homes/:homeId/users",
+  removeHomeUser: "/homes/:homeId/users/:userId",
 } as const;
