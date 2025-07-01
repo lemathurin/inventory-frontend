2ca62e9dad1508208dbbf5f4be55979a6776ee24
Pierre Pedrono <146751236+PierrePedrono@users.noreply.github.com>
2025-07-02 00:43:58 +0200

    fix:next_public_base_url

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,9 @@
 export const API_BASE_URL: string =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://78.47.140.225:3001";
+  process.env.NEXT_PUBLIC_API_BASE_URL || 
+  (process.env.NODE_ENV === 'production' 
+    ? "http://78.47.140.225:3001" 
+    : "http://localhost:4000");
+
 
 export const apiUrl = (path: string): string => {
   const apiPath = path.startsWith("/api") ? path : `/api${path}`;
