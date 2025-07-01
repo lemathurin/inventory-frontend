d4daa7f2e6ce78972aa60845acce46d3f13ab43c
Pierre Pedrono <146751236+PierrePedrono@users.noreply.github.com>
2025-07-01 22:57:21 +0200

    fix/vps-url-branch-name-on-ci

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL: string =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:4000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://78.47.140.225:3001";
 
 export const apiUrl = (path: string): string => {
   const apiPath = path.startsWith("/api") ? path : `/api${path}`;
